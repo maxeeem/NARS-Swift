ff15155e0dd4bed4a699cfaef87fb2922ac8268b
Maxim Tarasov <maxeeem@users.noreply.github.com>
2022-05-11 12:12:12 -0700

    adding Theorems.swift to git

---
+++ Code.playground/Sources/NAL/Theorems.swift
@@ -0,0 +1,49 @@
+
+
+public typealias Theorem = (Term) -> Statement?
+
+public enum Theorems: CaseIterable {
+    case inheritance
+    case implication
+    case equivalence
+}
+
+
+extension Theorems {
+    static func apply(_ j: Judgement) -> [Judgement] {
+        let res: [[Statement]] = self.allCases.map {
+            // TODO: possibly add conversion rule here?
+            var results = $0.rules.compactMap { $0(j.statement) }
+            if case .statement(let s, let c, let p) = j.statement, c == .similarity || c == .equivalence {
+                results.append(contentsOf: $0.rules.compactMap { $0(.statement(p, c, s)) })
+            }
+            return results
+        }
+
+        let results: [[Judgement]] = res.flatMap{$0}.map { t in
+            var results = Rules.strong.flatMap {
+                $0.apply((j, t-*(1,reliance)))
+            }.compactMap { $0 }
+            
+            if case .statement(let s, let c, let p) = j.statement, c == .similarity || c == .equivalence {
+                results.append(contentsOf:
+                    Rules.strong.flatMap {
+                        $0.apply((Judgement(.statement(p, c, s), j.truthValue), t-*(1,reliance)))
+                    }.compactMap { $0 }
+               )
+            }
+            return results
+        }
+        
+        let unique = Dictionary(grouping: results.flatMap({$0})) {
+            $0.statement.description
+        }.values.compactMap {
+            $0.max { j1, j2 in
+                let j = choice(j1: j1, j2: j2)
+                return j.statement == j2.statement
+            }
+        }
+        
+        return unique
+    }
+}
