faf90b8f485526443705f1f94c16b44ac03245b2
Maxim Tarasov <maxeeem@users.noreply.github.com>
2023-04-12 10:40:32 -0700

    setting up browser app

--- TokamakApp/Package.swift
+++ TokamakApp/Package.swift
@@ -7,14 +7,23 @@ let package = Package(
         .executable(name: "TokamakApp", targets: ["TokamakApp"])
     ],
     dependencies: [
-        .package(url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0")
+        .package(url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0"),
+        .package(path: "../../NARS-Swift")
     ],
     targets: [
         .executableTarget(
             name: "TokamakApp",
             dependencies: [
-                .product(name: "TokamakShim", package: "Tokamak")
-            ]),
+                .product(name: "TokamakShim", package: "Tokamak"),
+                .product(name: "NARS", package: "NARS-Swift"),
+                .product(name: "Narsese", package: "NARS-Swift")
+            ],
+            linkerSettings: [
+                .unsafeFlags(
+                    ["-Xlinker", "-z", "-Xlinker", "stack-size=131072"]
+                )
+            ]
+        ),
         .testTarget(
             name: "TokamakAppTests",
             dependencies: ["TokamakApp"]),
--- TokamakApp/Tests/TokamakAppTests/TokamakAppTests.swift
+++ TokamakApp/Tests/TokamakAppTests/TokamakAppTests.swift
@@ -6,6 +6,6 @@ final class TokamakAppTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TokamakApp().text, "Hello, World!")
+//        XCTAssertEqual(TokamakApp().text, "Hello, World!")
     }
 }
